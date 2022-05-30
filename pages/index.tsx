7178f5077f143400f39f39de7e9178794b64f3cb
Rostan <allowed_g@hotmail.com>
2022-05-29 22:19:26 -0500

    Cognito user login is implemented using NextAuth.js library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1 className="text-3xl font-bold underline">
-      Hello Next.js 👋
-    </h1>
+  <Layout title="Home">
+    <div className={"container"}>
+      <h1 className="text-3xl font-bold underline">
+        Hello from the Home page 👋 !
+      </h1>
+    </div>
   </Layout>
 )
 
-export default IndexPage
+export default IndexPage;
