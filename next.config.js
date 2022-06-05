090012d06a62e2806ea07ab0a58636f1ac516eb4
Rostan <allowed_g@hotmail.com>
2022-06-04 22:53:36 -0500

    hotfix/user-login - next.config.js file is included.

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    COGNITO_CLIENT_SECRET: process.env.COGNITO_CLIENT_SECRET,
+    COGNITO_ISSUER: process.env.COGNITO_ISSUER,
+    NEXT_PUBLIC_SECRET: process.env.NEXT_PUBLIC_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL
+  },
+};
